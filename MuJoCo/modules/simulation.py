8d2ed7e36499e0dc9dbe60ae3775e601cb68ad1b
Moses C. Nah <mosesnah@mit.edu>
2021-06-29 10:56:19 -0400

    quick update

--- MuJoCo/modules/simulation.py
+++ MuJoCo/modules/simulation.py
@@ -92,7 +92,7 @@ class Simulation( ):
             self.fps         = 60                                               # Frames per second for the mujoco render
             self.dt          = self.mjModel.opt.timestep                        # Time step of the simulation [sec]
             self.sim_step    = 0                                                # Number of steps of the simulation, in integer [-]
-            self.update_rate = round( 1 / self.dt / self.fps )                  # 1/dt = number of steps N for 1 second simulaiton, dividing this with frames-per-second (fps) gives us the frame step to be updated.
+            self.update_rate = round( 1 / self.dt / self.fps / 5 )              # 1/dt = number of steps N for 1 second simulaiton, dividing this with frames-per-second (fps) gives us the frame step to be updated.
             self.g           = self.mjModel.opt.gravity                         # Calling the gravity vector of the simulation environment
 
             # Saving additional model parameters for multiple purposes
@@ -303,14 +303,7 @@ class Simulation( ):
 
                     my_print(  geomXYZPositions  = self.mjData.geom_xpos[ self.idx_geom_names ],
                                geomXYZVelocities = self.mjData.geom_xvelp[ self.idx_geom_names ],
-                               jointAngleActual  = self.mjData.qpos[ : ],
-                                 jointVelActual  = self.mjData.qvel[ : ],
-                                 jointAccActual  = self.mjData.qacc[ : ],
-                                             x0  = self.controller.x0,
-                                            dx0  = self.controller.dx0,
-                                              M  = self.controller.Mmat,
-                                       jacobian  = self.mjData.get_geom_jacp(  "geom_EE"  ),
-                                       forceVec  = np.dot( self.mjData.get_body_xmat( "node1" ), self.mjData.sensordata ) ,
+                                      outputVal  = self.obj_func( self.mjModel, self.mjData ),
                                        inputVal  = input,file = file  )
 
 
