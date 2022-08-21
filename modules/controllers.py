a9e5dc72dba52d6b263fe0a1483ed84b46c40d71
Moses C. Nah <mosesnah@mit.edu>
2022-08-20 20:51:23 -0400

    Added functions for saving the data
    # Updates
    - Added the `scipy.io` function to save the data as `.mat` file which facilitates the data-saving process.

--- modules/controllers.py
+++ modules/controllers.py
@@ -1,4 +1,6 @@
-import numpy  as np
+from xmlrpc.client import boolean
+import numpy    as np
+import scipy.io
 from   modules.utils     import *
 from   modules.MLmodule  import *
 from   modules.constants import Constants as C
@@ -11,11 +13,12 @@ class Controller:
 
     """
 
-    def __init__( self, mj_model, mj_data, args, t_start: float ): 
+    def __init__( self, mj_sim, args, t_start: float ): 
 
         # Saving the reference of mujoco model and data for 
-        self.mj_model = mj_model
-        self.mj_data  = mj_data
+        self.mj_sim   = mj_sim
+        self.mj_model = mj_sim.mj_model
+        self.mj_data  = mj_sim.mj_data
 
         # Saving the arguments passed via ArgumentParsers
         self.mj_args  = args
@@ -128,9 +131,9 @@ class JointImpedanceController( Controller ):
 
     """
 
-    def __init__( self, mj_model, mj_data, mj_args, t_start: float = 0 ):
+    def __init__( self, mj_sim, mj_args, t_start: float = 0 ):
 
-        super( ).__init__( mj_model, mj_data, mj_args, t_start )
+        super( ).__init__( mj_sim, mj_args, t_start )
 
         # Getting the number of actuators for the tau_G calculation 
         self.n_act = len( self.mj_model.actuator_names )
@@ -140,6 +143,9 @@ class JointImpedanceController( Controller ):
 
         # Define the parameters that we will use for printing. This will be useful for "printing out the variables' detail" 
         self.print_par_naems = [ "K", "B", "q0" ]
+
+        # Saving the data as an array
+        self.is_save_data = mj_args.save_data
 
         if   self.n_act == 2:   
             c = 0.1
@@ -155,7 +161,7 @@ class JointImpedanceController( Controller ):
         self.traj_pos, self.traj_vel = None, None
 
         # The movement parameters
-        self.q0i, self.q0f, self.D = None, None, None
+        self.q0i, self.q0f, self.D, self.ti  = None, None, None, None
 
         # The number of control parameters of each name               
         # For K and B, we need self.n_act ** 2 parameters, in 2D
@@ -163,12 +169,40 @@ class JointImpedanceController( Controller ):
         # For D, we need a single scalar
         self.n_ctrl_pars    = [ self.n_act ** 2, self.n_act ** 2, self.n_act, self.n_act, 1 ]    
 
-    def set_traj( self, mov_pars: dict, basis_func: str = "min_jerk_traj" ):
+        # If save data is True, define the array for saving the data 
+        if self.is_save_data:
 
+            # The number of samples required is simply as follows
+            Ns = int( mj_args.run_time * mj_args.print_freq ) + 1
+
+            # The time array 
+            self.t_arr = np.zeros( Ns )
+
+            # The Torque input array
+            self.tau_arr = np.zeros( ( self.n_act, Ns ) )            
+    
+            # The current q (and q0) which is the joint (zero-torque) posture
+            self.q_arr  = np.zeros( ( self.n_act, Ns ) )            
+            self.q0_arr = np.zeros( ( self.n_act, Ns ) )                        
+
+            # The current qdot (and q0dot) which is the joint (zero-torque) velocity 
+            self.dq_arr  = np.zeros( ( self.n_act, Ns ) )                        
+            self.dq0_arr = np.zeros( ( self.n_act, Ns ) )                        
+
+            # The Jacobian matrix of the end-effector
+            self.Jp_arr = np.zeros( ( 3, self.n_act, Ns ) )
+            self.Jr_arr = np.zeros( ( 3, self.n_act, Ns ) )
+
+            # The index for saving the data
+            self.idx_data = 0                                    
+
+
+    def set_traj( self, mov_pars: dict, basis_func: str = "min_jerk_traj" ):
 
         self.q0i = mov_pars[ "q0i" ]
         self.q0f = mov_pars[ "q0f" ]
         self.D   = mov_pars[  "D"  ]
+        self.ti  = mov_pars[  "ti"  ]
 
         if   basis_func == "min_jerk_traj":
             self.traj_pos = lambda t :      self.q0i + ( self.q0f - self.q0i ) * (  10 * ( t / self.D ) ** 3 - 15 * ( t / self.D ) ** 4 +  6 * ( t / self.D ) ** 5 )
@@ -201,30 +235,68 @@ class JointImpedanceController( Controller ):
         q  = self.mj_data.qpos[ 0 : self.n_act ]       
         dq = self.mj_data.qvel[ 0 : self.n_act ]
  
-        if    t < self.t_start:
+        if    t < self.ti:
             self.q0  = self.q0i 
             self.dq0 = np.zeros( self.n_act )
 
-        elif  self.t_start < t <= self.t_start + self.D:
+        elif  self.ti < t <= self.ti + self.D:
             self.q0  = self.traj_pos( t - self.t_start )
             self.dq0 = self.traj_vel( t - self.t_start )
         else:
             self.q0  = self.q0f
             self.dq0 = np.zeros( self.n_act )
 
-        tau_imp = np.dot( self.K, self.q0 - q ) + np.dot( self.B, self.dq0 - dq )
+        tau_imp = self.K @ ( self.q0 - q ) + self.B @ ( self.dq0 - dq )
 
         tau_G = self.get_tau_G( )                     if is_gravity_comp else np.zeros( self.n_act ) 
         tau_n = np.random.normal( size = self.n_act ) if is_noise        else np.zeros( self.n_act ) 
 
         self.tau  = tau_imp + tau_G + tau_n
 
+        # Save the data if is_save_data Ture
+        if self.is_save_data:
+            
+            if self.mj_sim.n_steps % self.mj_sim.print_step == 0:
+
+                i = self.idx_data 
+
+                # The current time and joint posture 
+                self.t_arr[ i ] = t
+                self.q_arr[ :, i ]  =  q
+                self.dq_arr[ :, i ] = dq
+
+                # The zero-torque trajectory
+                self.q0_arr[ :, i ]  =  self.q0
+                self.dq0_arr[ :, i ] = self.dq0           
+
+                # The torque input 
+                self.tau_arr[ :, i ] = self.tau 
+
+                # The Jacobian matrix 
+                self.Jp_arr[ :, :, i ] = self.mj_data.get_site_jacp( "site_fore_arm_end" ).reshape( 3, -1 )
+                self.Jr_arr[ :, :, i ] = self.mj_data.get_site_jacr( "site_fore_arm_end" ).reshape( 3, -1 )
+
+                # Update the data pointer 
+                self.idx_data += 1
+
+
         # The  (1) object         (2) index array          (3) It's value. 
         return self.mj_data.ctrl, np.arange( self.n_act ), self.tau
 
-    def save_data( self ):
-        NotImplementedError( )
-
+    def save_data( self, dir_name ):
+        """
+            Save the controller details as a mat file
+        """
+        
+        file_name = dir_name + "/ctrl.mat"
+        
+        # [TODO] There will be a single liner to do this, including this in the parent class
+        scipy.io.savemat( file_name, { 'time': self.t_arr,    'qi': self.q0i,       'qf': self.q0f,
+                                          'D': self.D,        'ti': self.ti,        'q': self.q_arr, 
+                                         'dq': self.dq_arr,   'q0': self.q0_arr,  'dq0': self.dq0_arr, 
+                                        'tau': self.tau_arr, 'Jp' : self.Jp_arr,  'Jr' : self.Jr_arr,
+                                         'Kq': self.K,        'Bq': self.B }  )
+                
     def reset( self ):
         """
             Initialize all variables  
@@ -283,8 +355,8 @@ class CartesianImpedanceController( Controller ):
 
         tau_imp = np.dot( self.K, self.q0 - q ) + np.dot( self.B, self.dq0 - dq )
 
-        tau_G = self.get_tau_G( )                     if is_gravity_comp else np.zeros( self.n_act ) 
-        tau_n = np.random.normal( size = self.n_act ) if is_noise        else np.zeros( self.n_act ) 
+        tau_G = self.get_tau_G( )                     
+        tau_n = np.random.normal( size = self.n_act ) 
 
         self.tau  = tau_imp + tau_G + tau_n
 
